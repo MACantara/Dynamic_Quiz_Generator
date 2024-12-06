d0fa7ccd66bca2bb5dc584640fb18d0b39d5b4f7
Michael Angelo R. Cantara <cantara.michaelangelo@gmail.com>
2024-12-07 00:09:37 +0800

    Add host and port to config.py

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='0.0.0.0', port=5000, debug=app.config['DEBUG'])
+    app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.config['DEBUG'])
